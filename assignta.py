e542070741c154e488120bea4012c212dc6c8a4b
Aryan Jain <jain.aryan1@northeastern.edu>
2024-11-11 23:24:11 -0500

    monday

--- assignta.py
+++ assignta.py
@@ -5,6 +5,7 @@ assignta.py: An evolutionary computing framework.
 from evo import Evo
 import numpy as np
 import pandas as pd
+from collections import defaultdict
 
 def allocation(A, tas):
     
@@ -13,9 +14,19 @@ def allocation(A, tas):
 
 
 def conflicts(A, sections):
+
+    D = defaultdict(list)
     
     L = [(i, j) for i in range(len(A)) for j in range(len(A[i])) if A[i][j] == 1]
+
+    for i, j in L:
+        D[i].append(j)
     
+    
+
+
+
+
 
 def undersupport():
     pass
