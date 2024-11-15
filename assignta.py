48aace261a8adbbe03d5d8e9fb127910783293aa
Aryan Jain <jain.aryan1@northeastern.edu>
2024-11-15 12:17:04 -0500

    mutation agent

--- assignta.py
+++ assignta.py
@@ -44,6 +44,15 @@ def unpreferred(A):
 
     return sum([A[i][j] for i in range(len(A)) for j in range(len(A[i])) if A[i][j] == 1 and tas.loc[i, str(j)] == 'W'])
 
+def mutation(solutions, mutation_rate=0.3):
+
+    sol = solutions[0]
+    rows, cols = len(sol), len(sol[0])
+
+    mutated_sol = [[1 - sol[i][j] if rnd.random() < mutation_rate else sol[i][j] for j in range(cols)] for i in range(rows)]
+
+    return mutated_sol
+
 
 @profile
 def main():
@@ -56,9 +65,9 @@ def main():
     E.add_fitness_criteria("unwilling", unwilling)
     E.add_fitness_criteria("unpreferred", unpreferred)
 
-    for _ in range(10):
-        base_sol = [[0 for _ in range(17)] for _ in range(43)]
-        E.add_solution(base_sol)
+
+    base_sol = [[0 for _ in range(17)] for _ in range(43)]
+    E.add_solution(base_sol)
 
     E.evolve(n=10000, dom=100, status=1000)
 
