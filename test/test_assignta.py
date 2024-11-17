4e90e7c44c58f8c521a4169b73a5fe99249354c5
Aryan Jain <jain.aryan1@northeastern.edu>
2024-11-16 20:38:43 -0500

    finished test cases

--- test/test_assignta.py
+++ test/test_assignta.py
@@ -27,44 +27,46 @@ def C():
     return df.values.tolist()
 
 @pytest.fixture
-def sections():
-    return pd.read_csv('data/sections.csv')
-
-@pytest.fixture
-def tas():
-    return pd.read_csv("data/tas.csv")
+def D():
+    df = pd.read_csv('solutions/solution_matrix.csv', header=None)
+    return df.values.tolist()
 
 
-def test_allocation(A, B, C, tas):
+def test_allocation(A, B, C, D):
     
-    assert allocation(A, tas) == 37, "Expected 37, but got a different output."
-    assert allocation(B, tas) == 41, "Expected 41, but got a different output."
-    assert allocation(C, tas) == 23, "Expected 23, but got a different output."
+    assert allocation(A) == 37, "Expected 37, but got a different output."
+    assert allocation(B) == 41, "Expected 41, but got a different output."
+    assert allocation(C) == 23, "Expected 23, but got a different output."
+    assert allocation(D) == 0, "Expected 0, but got a different output."
 
 
-def test_conflicts(A, B, C, sections):
+def test_conflicts(A, B, C, D):
     
-    assert conflicts(A, sections) == 8, "Expected 8, but got a different output."
-    assert conflicts(B, sections) == 5, "Expected 5, but got a different output."
-    assert conflicts(C, sections) == 2, "Expected 2, but got a different output."
+    assert conflicts(A) == 8, "Expected 8, but got a different output."
+    assert conflicts(B) == 5, "Expected 5, but got a different output."
+    assert conflicts(C) == 2, "Expected 2, but got a different output."
+    assert conflicts(D) == 0, "Expected 0, but got a different output."
 
 
-def test_undersupport(A, B, C, sections):
+def test_undersupport(A, B, C, D):
    
-    assert undersupport(A, sections) == 1, "Expected 1, but got a different output."
-    assert undersupport(B, sections) == 0, "Expected 0, but got a different output."
-    assert undersupport(C, sections) == 7, "Expected 7, but got a different output."
+    assert undersupport(A) == 1, "Expected 1, but got a different output."
+    assert undersupport(B) == 0, "Expected 0, but got a different output."
+    assert undersupport(C) == 7, "Expected 7, but got a different output."
+    assert undersupport(D) == 1, "Expected 213, but got a different output."
 
 
-def test_unwilling(A, B, C, tas):
+def test_unwilling(A, B, C, D):
     
-    assert unwilling(A, tas) == 53, "Expected 53, but got a different output."
-    assert unwilling(B, tas) == 58, "Expected 58, but got a different output."
-    assert unwilling(C, tas) == 43, "Expected 43, but got a different output."
+    assert unwilling(A) == 53, "Expected 53, but got a different output."
+    assert unwilling(B) == 58, "Expected 58, but got a different output."
+    assert unwilling(C) == 43, "Expected 43, but got a different output."
+    assert unwilling(D) == 0, "Expected 0, but got a different output."
 
-def test_unpreferred(A, B, C, tas):
+def test_unpreferred(A, B, C, D):
     
-    assert unpreferred(A, tas) == 15, "Expected 15, but got a different output."
-    assert unpreferred(B, tas) == 19, "Expected 19, but got a different output."
-    assert unpreferred(C, tas) == 10, "Expected 10, but got a different output."
+    assert unpreferred(A) == 15, "Expected 15, but got a different output."
+    assert unpreferred(B) == 19, "Expected 19, but got a different output."
+    assert unpreferred(C) == 10, "Expected 10, but got a different output."
+    assert unpreferred(D) == 3, "Expected 3, but got a different output."
 
